99bc0e6299e9f2346429515781465b6b95d0b0d2
Matt Roy <Matt Roy>
2017-04-19 01:34:27 -0400

    first commit

---
+++ ActivityMonitor/SeinfeldThemeNotification.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.Media.Playback;
+using Windows.Devices.Gpio;
+using Windows.System.Threading;
+
+namespace ActivityMonitor
+{
+    class SeinfeldThemeNotification
+    {
+        public async Task NotifyAsync()
+        {
+            StorageFile file = await StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appx:///Assets/Seinfeld_transition_01.mp3"));
+            MediaPlayer player = BackgroundMediaPlayer.Current;
+            player.AutoPlay = false;
+            player.SetFileSource(file);
+            player.Volume = 100;
+            player.Play();
+        }
+    }
+}
---
+++ ActivityMonitor/StartupTask.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Http;
+using Windows.ApplicationModel.Background;
+
+namespace ActivityMonitor
+{
+    /// <summary>
+    /// Entry point into the app.
+    /// </summary>
+    public sealed class StartupTask : IBackgroundTask
+    {
+        private BackgroundTaskDeferral deferral;
+        private IMotionDetector motionDetector;
+        private SeinfeldThemeNotification themeNotification;
+
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            // Defer task instance in order to keep this task running
+            deferral = taskInstance.GetDeferral();
+            taskInstance.Canceled += TaskInstanceCanceled;
+            
+            motionDetector = new PassiveInfraredMotionDetector(26, 500);
+            motionDetector.Initialize();
+
+            themeNotification = new SeinfeldThemeNotification();
+
+            motionDetector.MotionDetected += MotionDetectorMotionDetectedAsync;
+        }
+
+        private void TaskInstanceCanceled(IBackgroundTaskInstance sender, BackgroundTaskCancellationReason reason)
+        {
+            motionDetector.Cleanup();
+        }
+
+        private async void MotionDetectorMotionDetectedAsync()
+        {
+            await themeNotification.NotifyAsync();
+        }
+    }
+}
